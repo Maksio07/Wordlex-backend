fa5c2c9cd27141395d347bbed06b3be25548d844
Maks <mmaksimov20000806@gmail.com>
2026-05-18 22:18:19 +0200

    db

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const { DBPass, DBHost, DBUser, PORT, SECRET_ACCESS_TOKEN, SECRET_SENDGRID_KEY } = process.env;
+const { DBPass, DBHost, DBUser, PORT, SECRET_ACCESS_TOKEN, SECRET_SENDGRID_KEY, DBName } = process.env;
 
-module.exports =  { DBPass, DBHost, DBUser, PORT, SECRET_ACCESS_TOKEN, SECRET_SENDGRID_KEY };
+module.exports =  { DBPass, DBHost, DBUser, PORT, SECRET_ACCESS_TOKEN, SECRET_SENDGRID_KEY, DBName };
