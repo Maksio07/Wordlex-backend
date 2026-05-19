91e1f2dba819816f21b7454c3ff7e9937a7f0d55
Maks <mmaksimov20000806@gmail.com>
2026-05-19 22:29:33 +0200

    g

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use('/profile', isAuth, wordsRouter)
 
 app.use((err, req, res, next) => {
 	console.error("!!! WYKRYTO BŁĄD NA BACKENDZIE !!!:", err);
-	console.log('Response:', res);
+	console.log('Response answer:', res);
 	if (err.code === 'EBADCSRFTOKEN') {
 		return res.status(403).json({
 			status: 'error',
