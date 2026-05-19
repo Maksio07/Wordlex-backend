91674acbdc98a42cf88f297840522f6f50c6dd6a
Maks <mmaksimov20000806@gmail.com>
2026-05-19 22:50:20 +0200

    db

--- util/database.js
+++ util/database.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2');
-const {DBHost, DBPass, DBUser} = require('../config/index')
+const mysql = require('mysql2')
+const { DBHost, DBPass, DBUser, DBName, DBPort } = require('../config/index')
 
 const pool = mysql.createPool({
-    host: DBHost,
-    user: DBUser,
-    database: 'wordlex',
-    password: DBPass
+	host: DBHost,
+	user: DBUser,
+	database: DBName,
+	password: DBPass,
+	port: DBPort,
 })
 
-module.exports = pool.promise();
+module.exports = pool.promise()
